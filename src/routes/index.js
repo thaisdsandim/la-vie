cab3b1bc6a06ba36a9ba77de834852e57f6f73ef
MonicaIacy <monicamendes81@yahoo.com>
2022-09-15 20:08:08 -0300

    feat: pacientes routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,28 @@
 const express = require("express")
 
 const psicologoController = require("../controllers/psicologo.controller")
+const pacienteController = require("../controllers/paciente.controller")
 const AtendimentosController = require("../controllers/atendimento.controller");
 const painel = require('../controllers/painel.controller');
 
 const psicologosCreateValidation = require("../validations/psicologos/create")
 const psicologosUpdateValidation = require("../validations/psicologos/update")
+const pacientesCreateValidation = require("../validations/pacientes/create")
+const pacientesUpdateValidation = require("../validations/pacientes/update")
 
 const routes = express.Router()
 
-routes.get("/psicologo", psicologoController.listarPsicologos)
-routes.get("/psicologo/:id", psicologoController.getOne)
-routes.post("/psicologo", psicologosCreateValidation, psicologoController.cadastrarPsicologo)
-routes.put("/psicologo/:id", psicologosUpdateValidation, psicologoController.atualizarCadastro)
-routes.delete("/psicologo/:id", psicologoController.deletarPsicologo)
+routes.get("/psicologos", psicologoController.listarPsicologos)
+routes.get("/psicologos/:id", psicologoController.getOne)
+routes.post("/psicologos", psicologosCreateValidation, psicologoController.cadastrarPsicologo)
+routes.put("/psicologos/:id", psicologosUpdateValidation, psicologoController.atualizarCadastro)
+routes.delete("/psicologos/:id", psicologoController.deletarPsicologo)
+
+routes.get("/pacientes", pacienteController.listarPacientes)
+routes.get("/pacientes/:id", pacienteController.getOne)
+routes.post("/pacientes", pacientesCreateValidation, pacienteController.cadastrarPaciente)
+routes.put("/pacientes/:id", pacientesUpdateValidation, pacienteController.atualizarCadastro)
+routes.delete("/pacientes/:id", pacienteController.deletarPaciente)
 
 routes.get('/atendimentos', AtendimentosController.listAttendances);
 routes.get('/atendimentos/:id', AtendimentosController.listOfAttendance);
